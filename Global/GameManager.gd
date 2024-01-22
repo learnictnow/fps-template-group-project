bc5f29a0b63a3777c18abd47c763063f5f8883df
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2024-01-22 23:07:12 +1300

    Build working FPS player with animations for sounds and weapon

---
+++ Global/GameManager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var previous_scene: String
+var final_time: float = 0.0
+var final_deaths: int = 0
+@export var game_length = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
